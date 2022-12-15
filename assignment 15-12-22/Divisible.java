12162e1957eb9adc2c2c6a2ec733dfee491c06ed
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2022-12-15 15:21:12 +0530

    Create Divisible.java

---
+++ assignment 15-12-22/Divisible.java
@@ -0,0 +1,24 @@
+//WAP to check whether a number is divisible by both 5 and 11
+
+package basic;
+
+import java.util.Scanner;
+
+public class Divisible {
+	
+	public static void main(String[] args) {
+		int num;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number:");
+		num = sc.nextInt();
+		if(num%5==0 && num%11==0)
+		{
+			System.out.println("The no. is divisible by both 5 and 11");
+		}
+		else
+		{
+			System.out.println("The no. is not divisible by both 5 and 11");
+		}
+	}
+
+}
