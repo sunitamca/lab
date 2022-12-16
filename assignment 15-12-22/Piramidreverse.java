216dc606400dd27798fb138f05663f05caf5ee1d
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2022-12-16 15:08:08 +0530

    Create Piramidreverse.java

---
+++ assignment 15-12-22/Piramidreverse.java
@@ -0,0 +1,19 @@
+//WAP to print inverted pyramidreverse
+
+package basic;
+
+public class Piramidreverse {
+
+	public static void main(String[] args) {
+		for(int i=5;i>=1;i--)
+		{
+			for(int j=1;j<=i;j++)
+			{
+				System.out.print("* ");
+			}
+				System.out.println();//new line
+		}
+
+	}
+
+}
