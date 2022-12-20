f23731e55c03cc5215ece9ebf94a777eee1ba487
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2022-12-20 22:51:04 +0530

    Create FirstLastDigit.java

---
+++ Assignment 20-12-2022/FirstLastDigit.java
@@ -0,0 +1,23 @@
+package basic;
+
+import java.util.Scanner;
+
+public class FirstLastDigit {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num,first_digit=0,last_digit=0,temp;
+		System.out.println("Enter a number:");
+		num=sc.nextInt();
+		temp=num;
+		last_digit =num%10;
+		System.out.println("The last digit of "+num+" is:"+last_digit);
+		for(;num>=10;)
+		{
+			num = num/10;
+			first_digit =num;
+		}
+		System.out.println("The first digit of "+temp+" is: "+first_digit);
+	}
+
+}
