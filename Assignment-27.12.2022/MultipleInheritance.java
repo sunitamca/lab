2a46bc9d03f34883e2be8f74f3dfc2d7e816248f
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2022-12-28 10:14:17 +0530

    Create MultipleInheritance.java

---
+++ Assignment-27.12.2022/MultipleInheritance.java
@@ -0,0 +1,26 @@
+package abstractiondemo;
+interface A{
+	public void msg();
+	
+}
+interface B{
+	public void msg();
+}
+ class C implements A,B{
+
+	@Override
+	public void msg() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
+public class MultipleInheritance {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
