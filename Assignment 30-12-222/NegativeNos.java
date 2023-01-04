35e1eec4389c410676c1bdc08ae647ec278887a9
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2023-01-04 16:31:46 +0530

    Create NegativeNos.java

---
+++ Assignment 30-12-222/NegativeNos.java
@@ -0,0 +1,33 @@
+package arrayprogames;
+
+import java.util.Scanner;
+
+public class NegativeNos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of array:");
+		int s= sc.nextInt();
+		int a[] = new int[s];
+		System.out.println("Enter the elements:");
+		for(int i=0;i<s;i++)
+		{
+			a[i]= sc.nextInt();
+		}
+		System.out.println("Array:");
+		for(int i=0;i<s;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+		System.out.println();
+		System.out.println("Negative number are:");
+		for(int i=0;i<s;i++)
+		{
+			if(a[i]<0)
+			{
+				System.out.print(a[i]+" ");
+			}
+		}
+	}
+
+}
