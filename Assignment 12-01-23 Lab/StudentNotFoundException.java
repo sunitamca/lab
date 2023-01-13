7f79f0183fb000e7a25fecc50c7b2ac6af61b9c4
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2023-01-13 16:47:45 +0530

    Create StudentNotFoundException.java

---
+++ Assignment 12-01-23 Lab/StudentNotFoundException.java
@@ -0,0 +1,12 @@
+package batchapplication;
+
+	public class StudentNotFoundException extends RuntimeException{
+		
+		public StudentNotFoundException(String message)
+		{
+			super(message);
+		}
+		
+	}
+
+
