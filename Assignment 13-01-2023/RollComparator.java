f69f1e4f2d1c7fa9ac356fe772ee7ba0434b971d
Sunita kumari <115772673+sunitamca@users.noreply.github.com>
2023-01-13 17:00:37 +0530

    Create RollComparator.java

---
+++ Assignment 13-01-2023/RollComparator.java
@@ -0,0 +1,18 @@
+package batchapplication;
+
+import java.util.Comparator;
+
+public class RollComparator implements Comparator<Student> {
+
+	@Override
+	public int compare(Student o1, Student o2) {
+		
+		if(o1.roll==o2.roll)
+		return 0;
+		else if(o1.roll>o2.roll)
+			return 1;
+		else
+			return -1;
+	}
+
+}
